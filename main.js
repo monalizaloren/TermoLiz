61a571ca874ef1b3fdca836c26305c33ebc9accc
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-19 09:34:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 function insertText(character) {
     document.getElementById("caixadetexto").value += character;
   }
-const substantivos = ["carro", "amigo", "sonho", "ideia", "pizza", "sabor", "tigre", "verde", "serra", "voces", "praia", "fraco", "pedra", "vazio", "tampa"];
-const indexAleatorio = Math.floor(Math.random() * substantivos.length);
-function gerarSubstantivoAleatorio() {
-    const substantivoAleatorio = substantivos[indexAleatorio];
-    console.log(substantivoAleatorio)
-  }
-gerarSubstantivoAleatorio();
